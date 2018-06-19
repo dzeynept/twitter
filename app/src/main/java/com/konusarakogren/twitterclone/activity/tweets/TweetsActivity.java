47e4d20cc0d1c53e18eec08af6315ecd4184a70d
Burak Karabulut <buraksinankarabulut@gmail.com>
2018-06-19 13:12:19 +0300

    final commit

--- app/src/main/java/com/konusarakogren/twitterclone/activity/tweets/TweetsActivity.java
+++ app/src/main/java/com/konusarakogren/twitterclone/activity/tweets/TweetsActivity.java
@@ -1,8 +1,10 @@
 package com.konusarakogren.twitterclone.activity.tweets;
 
 
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -11,24 +13,23 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.konusarakogren.twitterclone.R;
-import com.konusarakogren.twitterclone.activity.TweetsResponseModel;
 import com.konusarakogren.twitterclone.activity.base.BaseActivity;
 import com.konusarakogren.twitterclone.activity.login.LoginActivity;
+import com.konusarakogren.twitterclone.activity.profile.ProfileActivity;
 import com.konusarakogren.twitterclone.adapter.TweetsRecyclerAdapter;
-import com.parse.GetCallback;
+import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 public class TweetsActivity extends BaseActivity implements View.OnClickListener {
@@ -51,36 +52,25 @@ public class TweetsActivity extends BaseActivity implements View.OnClickListener
 
     @Override
     protected void initViews() {
-       /* ParseObject newTweet = new ParseObject("Tweets");
-        newTweet.put("new_tweet", "this is new tweet");
-        newTweet.saveInBackground();*/
-        user = ParseUser.getCurrentUser();
 
-
-/*
-        final ParseQuery<ParseObject> query = ParseQuery.getQuery("Tweets");
-        query.getInBackground(user.getObjectId(), new GetCallback<ParseObject>() {
-            public void done(List<ParseObject> object, ParseException e) {
-                if (e == null) {
-                    adapter = new TweetsRecyclerAdapter();
-                    tweets_rcyclerview.setAdapter(adapter);
-
-                } else {
-                    // something went wrong
-                }
-            }
-        });*/
-
-        add_tweet_btn = findViewById(R.id.add_tweet_btn);
-        add_tweet_edt = findViewById(R.id.tweets_edt);
         fab = findViewById(R.id.tweets_fa_btn);
         fab.setOnClickListener(this);
-        add_tweet_btn.setOnClickListener(this);
         tweets_rcyclerview = findViewById(R.id.tweets_recycler_view);
         tweets_rcyclerview.setLayoutManager(new LinearLayoutManager(this));
+        tweets_rcyclerview.setLayoutManager(new LinearLayoutManager(this));
 
-
-
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("Tweets");
+        query.findInBackground(new FindCallback<ParseObject>() {
+            public void done(List<ParseObject> tweetsList, ParseException e) {
+                if (e == null) {
+                    adapter = new TweetsRecyclerAdapter(tweetsList);
+                    tweets_rcyclerview.setAdapter(adapter);
+                    Log.d("tweets", "Retrieved " + tweetsList.size() + " tweets");
+                } else {
+                    Log.d("tweets", "Error: " + e.getMessage());
+                }
+            }
+        });
     }
 
 
@@ -99,6 +89,11 @@ public class TweetsActivity extends BaseActivity implements View.OnClickListener
             startActivity(new Intent(TweetsActivity.this, LoginActivity.class));
             finish();
             return true;
+        }else if (id == R.id.action_profile){
+
+                startActivity(new Intent(TweetsActivity.this, ProfileActivity.class));
+                finish();
+                return true;
         }
         return super.onOptionsItemSelected(item);
     }
@@ -107,28 +102,46 @@ public class TweetsActivity extends BaseActivity implements View.OnClickListener
     public void onClick(View v) {
         int id = v.getId();
         if(id == R.id.tweets_fa_btn){
-            add_tweet_btn.setVisibility(View.VISIBLE);
-            add_tweet_edt.setVisibility(View.VISIBLE);
+            showTweetPopUp();
         }
-        if (id == R.id.add_tweet_btn) {
-            String string = add_tweet_edt.getText().toString();
-            Toast.makeText(TweetsActivity.this, "btn", Toast.LENGTH_SHORT).show();
+    }
 
-            ParseObject newTweet = new ParseObject("Tweets");
-            newTweet.put("tweets", string);
-            newTweet.put("userName", user.getUsername());
-            newTweet.saveInBackground();
-            newTweet.saveInBackground(new SaveCallback() {
-                @Override
-                public void done(ParseException e) {
-                    if (e == null)
-                    Toast.makeText(TweetsActivity.this, "Tweet is posted!", Toast.LENGTH_SHORT).show();
-                    else
-                        Log.d(TAG, String.valueOf(e));
+    private void showTweetPopUp(){
+
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.getWindow()
+                .setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+        dialog.setContentView(R.layout.pop_up_tweet);
+        dialog.setCancelable(false);
+
+        final Button btnSend = dialog.findViewById(R.id.btn_send);
+        final EditText tweetText = dialog.findViewById(R.id.textview_info_content);
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String string = tweetText.getText().toString();
+                if (string.equalsIgnoreCase("")) {
+                    Toast.makeText(TweetsActivity.this, "You cant send emty tweet!", Toast.LENGTH_SHORT).show();
+                    return;
                 }
-            });
-            add_tweet_btn.setVisibility(View.GONE);
-            add_tweet_edt.setVisibility(View.GONE);
-        }
+                ParseObject newTweet = new ParseObject("Tweets");
+                newTweet.put("tweets", string);
+                newTweet.put("userName", user.getUsername());
+                newTweet.saveInBackground();
+                newTweet.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if (e == null)
+                            Toast.makeText(TweetsActivity.this, "Tweet is posted!", Toast.LENGTH_SHORT).show();
+                        else
+                            Log.d(TAG, String.valueOf(e));
+                    }
+                });
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
     }
 }
--- app/src/main/java/com/konusarakogren/twitterclone/adapter/TweetsRecyclerAdapter.java
+++ app/src/main/java/com/konusarakogren/twitterclone/adapter/TweetsRecyclerAdapter.java
@@ -1,64 +1,59 @@
 package com.konusarakogren.twitterclone.adapter;
 
-import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.konusarakogren.twitterclone.R;
 import com.parse.ParseObject;
-import com.parse.ParseUser;
 
 import java.util.List;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
+public class TweetsRecyclerAdapter extends RecyclerView.Adapter {
+    List<ParseObject> tweetsList;
 
-public class TweetsRecyclerAdapter extends RecyclerView.Adapter<TweetsRecyclerAdapter.ViewHolder> {
-    List<String> tweetsList;
+    public TweetsRecyclerAdapter(List<ParseObject> tweetsList) {
+        this.tweetsList = tweetsList;
+    }
 
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
-        View v = layoutInflater.inflate(R.layout.tweets_recycler_item, parent, false);
-        return new ViewHolder(v);    }
+        return new TweetViewHolder(layoutInflater.inflate(R.layout.tweets_recycler_item, parent, false));
+    }
 
+    private TweetViewHolder mHolder;
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    public void onBindViewHolder(final RecyclerView.ViewHolder holder, final int position) {
+        this.mHolder = (TweetViewHolder) holder;
 
+        String a = tweetsList.get(position).getString("tweets");
+        mHolder.tweet.setText(tweetsList.get(position).getString("tweets"));
     }
 
     @Override
     public int getItemCount() {
-        return 3;
+        if (tweetsList == null)
+            return 0;
+        else
+            return tweetsList.size();
     }
 
+    @Override
+    public int getItemViewType(int position) {
+        return position;
+    }
 
-
-    class ViewHolder extends RecyclerView.ViewHolder {
+    class TweetViewHolder extends RecyclerView.ViewHolder {
         TextView tweet;
         TextView userName;
-        EditText tweet_edt;
-        public ViewHolder(View itemView) {
+        public TweetViewHolder(View itemView) {
             super(itemView);
             tweet = itemView.findViewById(R.id.tweets_txt);
             userName = itemView.findViewById(R.id.user_name_txt);
-            tweet_edt = itemView.findViewById(R.id.tweets_edt);
-
-            ParseUser user = ParseUser.getCurrentUser();
-            if (user != null) {
-                tweet.setText("tweet");
-                userName.setText("username");
-
-                //  tweetsList = user.get("tweets");
-               // tweetsList.add(String.valueOf(user.get("tweets")));
-
             }
-        }
     }
 }
